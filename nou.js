1eedf1c99a355b3c4857a3a961ddbd5c080c5fab
Nave0505 <38740166+Nave0505@users.noreply.github.com>
2018-04-25 23:45:06 -0400

    Create nou.js

---
+++ nou.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'no u') {
+    message.reply('https://www.google.com/search?q=uno+reverse+card&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiIys6vgdfaAhXPmeAKHZjPB-wQ_AUICigB&biw=1280&bih=703&safe=active&ssui=on#imgrc=Zyvdoual3fyA0M:');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
