8d9110a895cb15cdab4f05bc80b8a8bfae1d1638
Nave0505 <38740166+Nave0505@users.noreply.github.com>
2018-04-25 23:41:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,8 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  if (message.content === 'no u') {
+    message.reply('https://www.google.com/search?q=uno+reverse+card&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiIys6vgdfaAhXPmeAKHZjPB-wQ_AUICigB&biw=1280&bih=703&safe=active&ssui=on#imgrc=Zyvdoual3fyA0M:');
+  }
 client.login(process.env.BOT_TOKEN);
