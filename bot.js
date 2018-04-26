1ee3c4a8e49f5452cc2ffd03692e6508aa77738c
Nave0505 <38740166+Nave0505@users.noreply.github.com>
2018-04-26 00:00:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === 'your mom gay') {
-    message.reply('no u');
+  if (message.content === 'no u') {
+    message.reply('https://pbs.twimg.com/media/DQ1kynkW4AArXD9.jpg');
   }
 });
 
